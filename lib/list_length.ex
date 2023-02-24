e1663571dc7f6ae1a10cf19309e655dacd59cba1
Noronha1612 <inc.691@gmail.com>
2023-02-24 11:57:07 -0300

    Challenge result

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,15 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    getLength(list, 0)
+  end
+
+  defp getLength([], currentLength) do
+    currentLength
+  end
+
+  defp getLength([_head | tail], currentLength) do
+    currentLength = currentLength + 1
+
+    getLength(tail, currentLength)
   end
 end
